f82afd77aa2a9d2fe6529b8edc496a0298188193
Pezhman Zarabadi-Poor <pzarabadip@gmail.com>
2021-09-14 23:47:22 +0100

    ♻️ refactore orm imports in examples1-4

--- examples/example01.py
+++ examples/example01.py
@@ -4,15 +4,12 @@ import sys
 import click
 
 from aiida.common import NotExistent
-from aiida.plugins import DataFactory
 from aiida.engine import run_get_pk
 from aiida.plugins import CalculationFactory
-from aiida.orm import Code, Dict, Bool, Str, List
-
-SinglefileData = DataFactory('singlefile')
+from aiida import orm
 
 
-def example_01(code: Code):
+def example_01(code: orm.Code):
     """Prepare the builder to submit.
 
     Args:
@@ -20,7 +17,7 @@ def example_01(code: Code):
     """
 
     pwd = os.path.dirname(os.path.realpath(__file__))
-    structure = SinglefileData(file=os.path.join(pwd, 'test.cif'))
+    structure = orm.SinglefileData(file=os.path.join(pwd, 'test.cif'))
 
     SupercellCalculation = CalculationFactory('supercell')
 
@@ -28,19 +25,19 @@ def example_01(code: Code):
 
     builder.code = code
     builder.structure = structure
-    builder.charges = Dict(dict={'Ca*': 2, 'Al*': 3, 'Si*': 4, 'O*': -2})
-    builder.calculate_coulomb_energies = Bool(True)
-    builder.charge_balance_method = Str('yes')
-    builder.merge_symmetric = Bool('True')
-    builder.supercell_size = List(list=[1, 1, 2])
+    builder.charges = orm.Dict(dict={'Ca*': 2, 'Al*': 3, 'Si*': 4, 'O*': -2})
+    builder.calculate_coulomb_energies = orm.Bool(True)
+    builder.charge_balance_method = orm.Str('yes')
+    builder.merge_symmetric = orm.Bool('True')
+    builder.supercell_size = orm.List(list=[1, 1, 2])
     builder.metadata.options.resources = {  #pylint: disable = no-member
         'num_machines': 1,
         'num_mpiprocs_per_machine': 1,
     }
-    builder.save_as_archive = Bool(False)
+    builder.save_as_archive = orm.Bool(False)
     builder.metadata.options.max_wallclock_seconds = 1 * 30 * 60
 
-    builder.sample_structures = Dict(dict={
+    builder.sample_structures = orm.Dict(dict={
         'low_energy': 2,
         'high_energy': 2,
         'random': 2,
@@ -55,7 +52,7 @@ def example_01(code: Code):
 def cli(codelabel):
     """Click interface"""
     try:
-        code = Code.get_from_string(codelabel)
+        code = orm.Code.get_from_string(codelabel)
     except NotExistent:
         print("The code '{}' does not exist".format(codelabel))
         sys.exit(1)
--- examples/example02.py
+++ examples/example02.py
@@ -4,17 +4,14 @@ import sys
 import click
 
 from aiida.common import NotExistent
-from aiida.plugins import DataFactory
 from aiida.engine import run_get_pk
 from aiida.plugins import CalculationFactory
-from aiida.orm import Code, Dict, Bool, Str, List
+from aiida import orm
 
 from pymatgen.core import Structure
 
-StructureData = DataFactory('structure')
 
-
-def example_02(code: Code):
+def example_02(code: orm.Code):
     """Prepare the builder to submit.
 
     Args:
@@ -23,7 +20,7 @@ def example_02(code: Code):
 
     pwd = os.path.dirname(os.path.realpath(__file__))
     strc = Structure.from_file(os.path.join(pwd, 'test.cif'))
-    structure = StructureData(pymatgen_structure=strc)
+    structure = orm.StructureData(pymatgen_structure=strc)
 
     SupercellCalculation = CalculationFactory('supercell')
 
@@ -31,19 +28,19 @@ def example_02(code: Code):
 
     builder.code = code
     builder.structure = structure
-    builder.charges = Dict(dict={'Ca*': 2, 'Al*': 3, 'Si*': 4, 'O*': -2})
-    builder.calculate_coulomb_energies = Bool(True)
-    builder.charge_balance_method = Str('yes')
-    builder.merge_symmetric = Bool('True')
-    builder.supercell_size = List(list=[1, 1, 2])
+    builder.charges = orm.Dict(dict={'Ca*': 2, 'Al*': 3, 'Si*': 4, 'O*': -2})
+    builder.calculate_coulomb_energies = orm.Bool(True)
+    builder.charge_balance_method = orm.Str('yes')
+    builder.merge_symmetric = orm.Bool('True')
+    builder.supercell_size = orm.List(list=[1, 1, 2])
     builder.metadata.options.resources = {  #pylint: disable = no-member
         'num_machines': 1,
         'num_mpiprocs_per_machine': 1,
     }
-    builder.save_as_archive = Bool(False)
+    builder.save_as_archive = orm.Bool(False)
     builder.metadata.options.max_wallclock_seconds = 1 * 30 * 60
 
-    builder.sample_structures = Dict(
+    builder.sample_structures = orm.Dict(
         dict={
             'low_energy': 2,
             'high_energy': 2,
@@ -63,7 +60,7 @@ def example_02(code: Code):
 def cli(codelabel):
     """Click interface"""
     try:
-        code = Code.get_from_string(codelabel)
+        code = orm.Code.get_from_string(codelabel)
     except NotExistent:
         print("The code '{}' does not exist".format(codelabel))
         sys.exit(1)
